ce5140f6e725ff5843ccc9801ac4f1e8250a93d2
sslukess <lvss.dev@gmail.com>
2024-11-01 21:27:51 +0000

    Remove duplication of DB context registration

--- thoughtBubbles-server/Program.cs
+++ thoughtBubbles-server/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.AddNpgsqlDbContext<ThoughtBubblesContext>("postgresdb");
 
 // Configure database connection: 
 if (builder.Environment.IsDevelopment())
