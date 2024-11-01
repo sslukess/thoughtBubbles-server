4c5b3f82d9c4ec1443c3555ab910025ed155a4f0
sslukess <lvss.dev@gmail.com>
2024-11-01 21:02:28 +0000

    add production db connection + ef core install for github action

--- thoughtBubbles-server/Program.cs
+++ thoughtBubbles-server/Program.cs
@@ -9,6 +9,17 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.AddNpgsqlDbContext<ThoughtBubblesContext>("postgresdb");
 
+// Configure database connection: 
+if (builder.Environment.IsDevelopment())
+{
+    // local dev database
+    builder.AddNpgsqlDbContext<ThoughtBubblesContext>("postgresdb");
+}
+else
+{
+    builder.AddNpgsqlDbContext<ThoughtBubblesContext>("AZURE_POSTGRESQL_CONNECTIONSTRING");
+}
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
